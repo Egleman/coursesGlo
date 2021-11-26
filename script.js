e23200b93f518c73fb52f7c38a48f67e5237e6fe
Egleman <martsnov@hotmail.com>
2021-11-26 10:04:43 +0300

    Выполнение урока 2

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Первый проект';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 53;
+let rollback = 7;
+let fullPrice = 100;
+let adaptive = true;
 
-alert("Привет!");
-console.log("Пока!");
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+console.log(screens.length);
+console.log("Стоимость вёрстки экранов" + " " + screenPrice + " " + "рубля");
+console.log(screens.toLowerCase().split(', '));
+console.log(fullPrice * (rollback/100));
