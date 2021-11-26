76ccd76bdc314fdc23492524f545c65e72ffe8d9
Egleman <martsnov@hotmail.com>
2021-11-26 13:43:05 +0300

    исправил ошибки

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ console.log(typeof(title));
 console.log(typeof(fullPrice));
 console.log(typeof(adaptive));
 console.log(screens.length);
-console.log("Стоимость вёрстки экранов" + " " + screenPrice + " " + "рубля");
+console.log("Стоимость вёрстки экранов " + screenPrice +  " рубля");
 console.log(screens.toLowerCase().split(', '));
 console.log(fullPrice * (rollback/100));
